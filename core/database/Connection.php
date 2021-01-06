95b224778199fc7059e609c6e941ddb795d9b23c
ruud-hermans <75683432+ruud-hermans@users.noreply.github.com>
2021-01-06 19:11:38 +0100

    Update Connection.php

--- core/database/Connection.php
+++ core/database/Connection.php
@@ -10,8 +10,6 @@
         public static function make($config){
 
             try{
-                // return new PDO('mysql:host=127.0.0.1;dbname=cmsgor', 'ruud', 'poep');
-
                 return new PDO(
                     $config['connection'].';dbname='.$config['name'],
                     $config['username'],
